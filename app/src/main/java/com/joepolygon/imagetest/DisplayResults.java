5691811ae4176873e6d782a2ffef24d7ceb41055
Joe Pelz <joe.pelz@gmail.com>
2016-01-11 17:12:44 -0800

    Render settings menu configured.

--- app/src/main/java/com/joepolygon/imagetest/DisplayResults.java
+++ app/src/main/java/com/joepolygon/imagetest/DisplayResults.java
@@ -4,13 +4,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
-public class DisplayResults extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
+public class DisplayResults extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener, TextView.OnEditorActionListener {
     private ProgressBar pb;
     private TextView progressMessage;
 
@@ -21,7 +22,10 @@ public class DisplayResults extends AppCompatActivity implements SeekBar.OnSeekB
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        android.support.v7.app.ActionBar temp = getSupportActionBar();
+        if (temp != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
 
         pb = (ProgressBar) findViewById(R.id.progressRendering);
         progressMessage = (TextView) findViewById(R.id.progressMessage);
@@ -33,6 +37,15 @@ public class DisplayResults extends AppCompatActivity implements SeekBar.OnSeekB
         sb.setOnSeekBarChangeListener(this);
         sb = (SeekBar) findViewById(R.id.seekP);
         sb.setOnSeekBarChangeListener(this);
+
+        EditText et = (EditText) findViewById(R.id.valFrames);
+        et.setOnEditorActionListener(this);
+        et = (EditText) findViewById(R.id.valA);
+        et.setOnEditorActionListener(this);
+        et = (EditText) findViewById(R.id.valB);
+        et.setOnEditorActionListener(this);
+        et = (EditText) findViewById(R.id.valP);
+        et.setOnEditorActionListener(this);
     }
 
     public void onRender(View v) {
@@ -51,6 +64,81 @@ public class DisplayResults extends AppCompatActivity implements SeekBar.OnSeekB
         progressMessage.setText(temp);
     }
 
+    @Override
+    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+        String text = v.getText().toString();
+        SeekBar sb;
+        String tag = v.getTag().toString();
+        double val;
+        int frames;
+
+        switch (tag) {
+            case "frames":
+                frames = Integer.parseInt(text);
+                if (frames < 2) {
+                    frames = 2;
+                } else if (frames > 200) {
+                    frames = 200;
+                }
+                v.setText(String.valueOf(frames));
+
+                sb = (SeekBar) findViewById(R.id.seekFrames);
+                if ((frames-2) <= sb.getMax()) {
+                    sb.setProgress(frames - 2);
+                    updateProgressMessage();
+                }
+                break;
+            case "a":
+                val = Double.parseDouble(text);
+                if (val <= 0) {
+                    val = 0.001;
+                    v.setText(String.valueOf(val));
+                }
+
+                sb = (SeekBar) findViewById(R.id.seekA);
+                Log.v("DisplayResults", "val is " + val);
+                val = val * 1000 - 1;
+                Log.v("DisplayResults", "modified val is " + val);
+                Log.v("DisplayResults", "sb.getMax() is " + sb.getMax());
+                if (val <= sb.getMax()) {
+                    sb.setProgress((int)val);
+                }
+                break;
+            case "b":
+                val = Double.parseDouble(text);
+                if (val < 1) {
+                    val = 1;
+                } else if (val > 2) {
+                    val = 2;
+                }
+                v.setText(String.valueOf(val));
+
+                sb = (SeekBar) findViewById(R.id.seekB);
+                val = (val - 1) * 100;
+                if (val <= sb.getMax()) {
+                    sb.setProgress((int)val);
+                }
+                break;
+            case "P":
+                val = Double.parseDouble(text);
+                if (val < 0) {
+                    val = 0;
+                } else if (val > 1) {
+                    val = 1;
+                }
+                v.setText(String.valueOf(val));
+
+                sb = (SeekBar) findViewById(R.id.seekP);
+                val = val * 100;
+                if (val <= sb.getMax()) {
+                    sb.setProgress((int)val);
+                }
+                break;
+            default:
+                return false;
+        }
+        return true;
+    }
 
     @Override
     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
@@ -69,7 +157,7 @@ public class DisplayResults extends AppCompatActivity implements SeekBar.OnSeekB
             case "a":
                 //Log.v("DisplayResults", "'a' seekbar changed");
                 field = (EditText) findViewById(R.id.valA);
-                field.setText(String.valueOf((seekBar.getProgress() + 1) / 100.0 * 0.1));
+                field.setText(String.valueOf((seekBar.getProgress() + 1) / 1000.0));
                 break;
             case "b":
                 //Log.v("DisplayResults", "'b' seekbar changed");
